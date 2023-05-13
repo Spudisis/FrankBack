843f493254d939bf601e339b0677835e2cad6c1e
andrew <sanakurin@gmail.com>
2023-05-13 23:11:41 +0300

    added project router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
 const person = require("./person.route");
+const project = require("./project.router")
 
 router.use("/person", person);
-//router.use("/projects", )
+router.use("/projects", project)
 
 module.exports = router;
