1c718a5bdf89d91ca2b1bf7d4102ad2494f90def
andrew <sanakurin@gmail.com>
2023-05-13 22:27:41 +0300

    added methods to project-service

--- service/project-service.js
+++ service/project-service.js
@@ -1,4 +1,4 @@
-const { users, modelLink, refreshPassword } = require("../models/models");
+const { projects, modelLink, refreshPassword } = require("../models/models");
 const bcrypt = require("bcrypt");
 const uuid = require("uuid");
 const mailService = require("./mail-service");
@@ -6,7 +6,76 @@ const tokenService = require("./token-service");
 const ApiError = require("../exceptions/api-error");
 
 class ProjectService {
+    async createEmptyProject(projectName, statusAccess){
+        if(!projectName){
+            throw ApiError.BadRequest("project name not exist");
+        } else if(!statusAccess){
+            throw ApiError.BadRequest("project status access not exist");
+        }
+        console.log(`New project setup; name: ${projectName}, access: ${statusAccess ? 'public' : 'private'}`);
+        const randUID = uuid.v4().slice(0, 16);
+        const project = await projects.create({ uid: randUID, statusAccess: statusAccess, layout: '{}' });
+        console.log('Project: ' + project);
+        return randUID;
+    }
 
+    async deleteProject(projectUid){
+        if(!projectUid){
+            throw ApiError.BadRequest("project uid not exist");
+        }
+        console.log(`Setup project delete process; project uid: ${projectUid}`);
+        const result = await projects.destroy({
+            where: {uid: projectUid}
+        });
+        if(result){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    async overwriteProject(projectUid, newLayout){
+        if(!projectUid){
+            throw ApiError.BadRequest("project uid not exist");
+        }
+        console.log(`Setup project rewrite; project uid: ${projectUid}`);
+        const [result] = await projects.update(
+            {
+                layout: newLayout,
+            },
+            {
+                where: { uid: projectUid },
+            }
+        );
+        if(result){
+            console.log(`Updated rows: ${result}`);
+        }else{
+            console.log('Row not found');
+        }
+    }
+
+    async getProject(projectUid){
+        if (!projectUid) {
+            throw ApiError.BadRequest("project uid not exist");
+        }
+        console.log(`Setup get project; project uid: ${projectUid}`);
+        const project = await projects.findOne({ 
+            where: { uid: projectUid } 
+        });
+        console.log('Got project; ' + project);
+        if (!project) {
+            throw ApiError.BadRequest("Project not found");
+        }
+        return project;
+    }
+
+    async getPublicProjects(){
+        console.log(`Setup get public projects`);
+        const visibleProjects = await projects.findAll({
+            where: { statusAccess: true }
+        });
+        return visibleProjects;
+    }
 }
 
 module.exports = new ProjectService();
